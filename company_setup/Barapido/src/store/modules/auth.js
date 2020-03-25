df113daa86a4bdeb414a900c8523f759b2d4d0bd
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-03-25 21:34:21 +0800

    attached the new reseller listener on auth and re-auth

--- company_setup/Barapido/src/store/modules/auth.js
+++ company_setup/Barapido/src/store/modules/auth.js
@@ -32,6 +32,7 @@ const auth = {
 
 					commit('SET_USER', userData);
 					dispatch('stock_orders/LISTEN_TO_STOCK_ORDERS', null, { root: true });
+					dispatch('distributors/LISTEN_TO_NEW_REGISTRATIONS', null, { root: true });
 					return userData;
 				} catch (error) {
 					await AUTH.signOut();
@@ -48,6 +49,7 @@ const auth = {
 
 				commit('SET_USER', userData);
 				dispatch('stock_orders/LISTEN_TO_STOCK_ORDERS', null, { root: true });
+				dispatch('distributors/LISTEN_TO_NEW_REGISTRATIONS', null, { root: true });
 				return;
 			} catch (error) {
 				await AUTH.signOut();
@@ -83,6 +85,7 @@ const auth = {
 				commit('SET_USER', {});
 				// UNSUBSCRIBE TO ORDERS
 				dispatch('stock_orders/UNSUBSCRIBE_FROM_STOCK_ORDERS', {}, { root: true });
+				dispatch('distributors/UNSUBSCRIBE_FROM_NEW_RESELLERS', {}, { root: true });
 			} catch (error) {
 				throw error;
 			}
