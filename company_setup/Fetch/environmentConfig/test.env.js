773e43d915ebe1bb7f0179b0f002fa18fe11225f
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-04-21 16:15:22 +0800

    used cloud functions as middleware for lalamove api calls

--- company_setup/Fetch/environmentConfig/test.env.js
+++ company_setup/Fetch/environmentConfig/test.env.js
@@ -11,5 +11,5 @@ module.exports = merge(devEnv, {
   messagingSenderId: '"769658614979"',
   appId: '"1:769658614979:web:0c1b3f1f986ddc1c0cde76"',
   primaryColor: '"#C6863F"',
-  lalamoveURL: '"https://sandbox-rest.lalamove.com/v2"'
+  lalamoveURL: '"https://us-central1-fetch-dev-a1f39.cloudfunctions.net/lalamoveAPI"'
 })
