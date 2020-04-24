4a4f8784cd3d634339e05d9a78b9752f47e03d37
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-04-24 09:11:37 +0800

    created previous files from old Fetch directory to the new

---
+++ company_setup/Fetch 2/environmentConfig/prod.env.js
@@ -0,0 +1,12 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  apiKey: '""',
+  authDomain: '""',
+  databaseURL: '""',
+  projectId: '""',
+  storageBucket: '""',
+  messagingSenderId: '""',
+  appId: '""',
+  primaryColor: '""',
+  lalamoveURL: '"https://rest.lalamove.com/v2"'
+}
