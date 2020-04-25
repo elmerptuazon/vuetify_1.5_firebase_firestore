4a0c402c333dd3c4b8f0ee61e8c15b767f515a16
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-04-25 11:54:22 +0800

    replaced custom files for Avon

--- company_setup/Avon_new/environmentConfig/dev.env.js
+++ company_setup/Avon_new/environmentConfig/dev.env.js
@@ -3,13 +3,12 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  apiKey: '"AIzaSyDAxQ8dvBH2CQNkvoFuklqVRV76KaRyKJ0"',
-  authDomain: '"appselltestapp.firebaseapp.com"',
-  databaseURL: '"https://appselltestapp.firebaseio.com"',
-  projectId: '"appselltestapp"',
-  storageBucket: '"appselltestapp.appspot.com"',
-  messagingSenderId: '"671506060709"',
-  appId: '"1:671506060709:web:7bcc28481ba6b94d12044c"',
-  measurementId: '"G-XV2JER7LNT"',
-  primaryColor: '"#0E1E38"',
+  apiKey: '"AIzaSyAeMzGe0okkodRmZZCExzhn96Y6-V1766s"',
+  authDomain: '"avon-dev-b9664.firebaseapp.com"',
+  databaseURL: '"https://avon-dev-b9664.firebaseio.com"',
+  projectId: '"avon-dev-b9664"',
+  storageBucket: '"avon-dev-b9664.appspot.com"',
+  messagingSenderId: '"879985541901"',
+  appId: '"1:879985541901:web:f0aaf0ffddb56a05a46644"',
+  primaryColor: '"#ED008C"',
 })
