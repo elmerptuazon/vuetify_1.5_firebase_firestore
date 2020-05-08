b47efd1f337289bac1e6285d1c88785d95489716
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-05-08 14:42:45 +0800

    fixed typo in companySetup file

--- config/manualCompanySetup.js
+++ config/manualCompanySetup.js
@@ -32,7 +32,7 @@ const prompts = require('prompts');
             { title: 'Mother Nurture', value: 'MotherNurture' },
             { title: 'Potato Corner', value: 'PotatoCorner' },
             { title: 'Aquitek', value: 'Aquitek', disabled: true },
-            { title: 'Chicharooms', value: 'Chicharooms', disabled: trues },
+            { title: 'Chicharooms', value: 'Chicharooms', disabled: true },
             { title: 'Ever Bilena', value: 'EverBilena', disabled: true },
             { title: 'Mary Kay', value: 'MaryKay', disabled: true },
             { title: 'My Natural', value: 'MyNatural', disabled: true }
