dfcb2ddc2e8021f3cfef9a7aee78efd39b02eb34
China Lao <china.appsell@gmail.com>
2020-06-09 17:11:19 +0800

    created directory for Bente Silog

---
+++ company_setup/BenteSilog/environmentConfig/prod.env.js
@@ -0,0 +1,14 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyA1p43Hys8LeHoqINSFOItJDEHLmggy4C4"',
+  authDomain: '"potatocornerdev.firebaseapp.com"',
+  databaseURL: '"https://potatocornerdev.firebaseio.com"',
+  projectId: '"potatocornerdev"',
+  storageBucket: '"potatocornerdev.appspot.com"',
+  messagingSenderId: '"846971526982"',
+  appId: '"1:846971526982:web:027bf465e5be6cc53e8f47"',
+  storageCode: '"PotatoCorner"',
+  primaryColor: '"#119247"',
+  companyName: '"Potato Corner"',
+  accountManagementURL: '"https://us-central1-potatocornerdev.cloudfunctions.net/accounts"',
+}
