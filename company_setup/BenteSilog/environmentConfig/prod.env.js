a7fbb2c527109dfb0771cb6b159d6d5c89b9930f
China Lao <china.appsell@gmail.com>
2020-06-09 17:17:07 +0800

    adjusted environmental configs for Bente Silog

--- company_setup/BenteSilog/environmentConfig/prod.env.js
+++ company_setup/BenteSilog/environmentConfig/prod.env.js
@@ -8,7 +8,7 @@ module.exports = {
   messagingSenderId: '"846971526982"',
   appId: '"1:846971526982:web:027bf465e5be6cc53e8f47"',
   storageCode: '"PotatoCorner"',
-  primaryColor: '"#119247"',
-  companyName: '"Potato Corner"',
+  primaryColor: '"#ff9602"',
+  companyName: '"Bente Silog"',
   accountManagementURL: '"https://us-central1-potatocornerdev.cloudfunctions.net/accounts"',
 }
--- company_setup/BenteSilog/environmentConfig/test.env.js
+++ company_setup/BenteSilog/environmentConfig/test.env.js
@@ -11,7 +11,7 @@ module.exports = merge(devEnv, {
   messagingSenderId: '"846971526982"',
   appId: '"1:846971526982:web:027bf465e5be6cc53e8f47"',
   storageCode: '"PotatoCorner"',
-  primaryColor: '"#119247"',
-  companyName: '"Potato Corner"',
+  primaryColor: '"#ff9602"',
+  companyName: '"Bente Silog"',
   accountManagementURL: '"https://us-central1-potatocornerdev.cloudfunctions.net/accounts"'
 })
