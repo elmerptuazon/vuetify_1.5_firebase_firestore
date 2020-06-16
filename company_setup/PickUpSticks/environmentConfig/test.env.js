337e8b41fe08474c69fba68d5f478e26cd1152ce
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-06-16 13:28:32 +0800

    re-adjusted firebase config for Pick Up Sticks

--- company_setup/PickUpSticks/environmentConfig/test.env.js
+++ company_setup/PickUpSticks/environmentConfig/test.env.js
@@ -3,16 +3,15 @@ var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  projectId: '"bwl-group"',
-  appId: '"1:801256472722:web:89e179e4e560b2afe1264b"',
-  databaseURL: '"https://bwl-group.firebaseio.com"',
-  storageBucket: '"bwl-group.appspot.com"',
-  locationId: '"us-central"',
-  apiKey: '"AIzaSyAvy2VDqvAujezudvFgKjnd_TbKhi7irUs"',
-  authDomain: '"bwl-group.firebaseapp.com"',
-  messagingSenderId: '"801256472722"',
+  projectId: '"pickupsticks-dev"',
+  appId: '"1:6834088913:web:fce7c161cf1ed751da8ef0"',
+  databaseURL: '"https://pickupsticks-dev.firebaseio.com"',
+  storageBucket: '"pickupsticks-dev.appspot.com"',
+  apiKey: '"AIzaSyDIzeMplcaHQoy0bl3revSAoLX_5o3LmTU"',
+  authDomain: '"pickupsticks-dev.firebaseapp.com"',
+  messagingSenderId: '"6834088913"',
   storageCode: '"PickUpSticks"',
   primaryColor: '"#f78e20"',
   companyName: '"Pick Up Sticks"',
-  accountManagementURL: '"https://us-central1-bwl-group.cloudfunctions.net/accounts"'
+  accountManagementURL: '"https://us-central1-pickupsticks-dev.cloudfunctions.net/accounts"'
 })
