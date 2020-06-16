b286306845118ea01b8597a1f240930df2c47387
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-06-16 13:25:09 +0800

    updated firebase config for Frito King

--- company_setup/FritoKing/environmentConfig/prod.env.js
+++ company_setup/FritoKing/environmentConfig/prod.env.js
@@ -1,15 +1,14 @@
 module.exports = {
   NODE_ENV: '"production"',
-  projectId: '"my-natural-dev"',
-  appId: '"1:943759044147:web:05877f812940998c837d46"',
-  databaseURL: '"https://my-natural-dev.firebaseio.com"',
-  storageBucket: '"my-natural-dev.appspot.com"',
-  locationId: '"us-central"',
-  apiKey: '"AIzaSyAfTZ44pssPxImQQ3IoHfUNZpWkEd8Zt48"',
-  authDomain: '"my-natural-dev.firebaseapp.com"',
-  messagingSenderId: '"943759044147"',
+  projectId: '"fritoking-dev"',
+  appId: '"1:200493591511:web:086dcbfe7f53d720aef992"',
+  databaseURL: '"https://fritoking-dev.firebaseio.com"',
+  storageBucket: '"fritoking-dev.appspot.com"',
+  apiKey: '"AIzaSyAKjz6J06tTgQHM4kqnTod45eOkXR9_NOQ"',
+  authDomain: '"fritoking-dev.firebaseapp.com"',
+  messagingSenderId: '"200493591511"',
   storageCode: '"FritoKing"',
   primaryColor: '"#c11515"',
   companyName: '"Frito King"',
-  accountManagementURL: '"https://us-central1-my-natural-dev.cloudfunctions.net/accounts"'
+  accountManagementURL: '"https://us-central1-fritoking-dev.cloudfunctions.net/accounts"'
 }
