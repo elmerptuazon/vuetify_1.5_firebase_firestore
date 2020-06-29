c148b14e60a03a202b6e9ae6ac542f66d029e0c7
China Lao <china.appsell@gmail.com>
2020-06-29 13:54:36 +0800

    adjusted the color, company name and storage code in the environmental configs

--- company_setup/BuffaloWingsNThings/environmentConfig/prod.env.js
+++ company_setup/BuffaloWingsNThings/environmentConfig/prod.env.js
@@ -7,8 +7,8 @@ module.exports = {
   apiKey: '"AIzaSyAKjz6J06tTgQHM4kqnTod45eOkXR9_NOQ"',
   authDomain: '"fritoking-dev.firebaseapp.com"',
   messagingSenderId: '"200493591511"',
-  storageCode: '"FritoKing"',
-  primaryColor: '"#c11515"',
-  companyName: '"Frito King"',
+  storageCode: '"BuffaloWingsNThings"',
+  primaryColor: '"#003784"',
+  companyName: '"Buffalo\'s Wings N Things"',
   accountManagementURL: '"https://us-central1-fritoking-dev.cloudfunctions.net/accounts"'
 }
