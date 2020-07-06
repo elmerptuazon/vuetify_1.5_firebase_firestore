149e5ddc5c789a996c0816b4597ecbaa17c20840
China Lao <china.appsell@gmail.com>
2020-07-06 17:43:21 +0800

    created directory for Matenara

---
+++ company_setup/Matenara/environmentConfig/dev.env.js
@@ -0,0 +1,15 @@
+var merge = require('webpack-merge')
+var prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  apiKey: '"AIzaSyDAxQ8dvBH2CQNkvoFuklqVRV76KaRyKJ0"',
+  authDomain: '"appselltestapp.firebaseapp.com"',
+  databaseURL: '"https://appselltestapp.firebaseio.com"',
+  projectId: '"appselltestapp"',
+  storageBucket: '"appselltestapp.appspot.com"',
+  messagingSenderId: '"671506060709"',
+  appId: '"1:671506060709:web:7bcc28481ba6b94d12044c"',
+  measurementId: '"G-XV2JER7LNT"',
+  primaryColor: '"#0E1E38"',
+})
