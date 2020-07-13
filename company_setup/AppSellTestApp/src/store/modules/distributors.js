d19c82fda01d30994bbb4faa2d57867fcc5631bb
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-07-13 12:07:03 +0800

    added actions that deletes a reseller and resends account verification to a reseller

--- company_setup/AppSellTestApp/src/store/modules/distributors.js
+++ company_setup/AppSellTestApp/src/store/modules/distributors.js
@@ -25,87 +25,54 @@ const distributors = {
         UPDATE_RESELLER(state, payload) {
             const index = state.resellersList.findIndex(reseller => reseller.id === payload.id);
             if(index !== -1) {
-                Object.keys(payload).forEach(key => {
-                    state.resellersList[index][key] = payload[key];
-                });
+                state.resellersList[index] = Object.assign({}, payload);
+                state.resellersList = [...state.resellersList];
+            }
+        },
+        REMOVE_RESELLER(state, payload) {
+            const index = state.resellersList.findIndex(reseller => reseller.id === payload.id);
+            if(index !== -1) {
+                state.resellersList.splice(index, 1);
+                state.resellersList = [...state.resellersList];
             }
         }
     },
     actions: {
+
         async LISTEN_TO_NEW_REGISTRATIONS({ state, commit, dispatch }) {
             commit('SET_RESELLERS_LIST', []);
-            let accountsToModify = [];
-
+            
             state.subscriber = DB.collection('accounts')
-                .where('type', '==', 'Reseller')
-                .orderBy('createdAt', 'desc')
-                .onSnapshot((snapshot) => {
-                    console.log('listening to new registrations');
+            .where('type', '==', 'Reseller')
+            .orderBy('createdAt', 'desc')
+            .onSnapshot((snapshot) => {
+                console.log('listening to new registrations');
 
-                    let changes = snapshot.docChanges();
+                let changes = snapshot.docChanges();
 
-                    changes = changes.map((change) => {
+                changes = changes.forEach((change) => {
 
-                        let data = change.doc.data();
-                        data.id = change.doc.id;
+                    let data = change.doc.data();
+                    data.id = change.doc.id;
 
-                        data.type = change.type;
-
-                        console.log(data);
-
-                        if(!data.hasOwnProperty('isRead') 
-                            && data.status.toLowerCase() === 'pending'
-                        ) {
-                            
-                            data.isRead = false;
-                            accountsToModify.push(data);
-
-                        } else if(!data.hasOwnProperty('isRead')) {
-                            data.isRead = true;
-                            accountsToModify.push(data);
-                        }
-
-                        return data;
-                    })
-
-                    if(!state.resellersList || !state.resellersList.length) {
-                        commit('SET_RESELLERS_LIST', changes);
+                    if(change.type === 'added') {
+                        console.log('added reseller: ', change);
+                        commit('ADD_RESELLER', data);
                     
-                    } else {
-                        changes.forEach((change) => {
-                            if(change.type === 'added' 
-                                && (!change.isRead || !change.hasOwnProperty('isRead'))
-                            ) {
-                                change.isRead = false;
-                                delete change.type;
-                                console.log('added reseller: ', change);
-                                // commit('ADD_RESELLER', change);
-                                state.resellersList.push(change);
-                            
-                            } else if(change.type === 'modified') {
-                                delete change.type;
-                                // commit('UPDATE_RESELLER', change);
-                                const index = state.resellersList.findIndex(reseller => reseller.id === change.id);
-                                if(index !== -1) {
-                                    state.resellersList[index] = Object.assign({}, change);
-                                    console.log('modified reseller: ', change);
-                                    dispatch('conversations/listenToConversations', null, {root:true});
-                                }
-                            }
-                        })
-                    } 
+                    } else if(change.type === 'modified') {
+                        console.log('modified reseller: ', data);
+                        commit('UPDATE_RESELLER', data);
+                        dispatch('conversations/listenToConversations', null, {root:true});
+                    
+                    } else if(change.type === 'removed') {
+                        console.log('removed reseller: ', data);
+                        commit('REMOVE_RESELLER', data);
+                    }
+
+                    return data;
                 })
 
-            if(accountsToModify.length) {
-                for(const account of accountsToModify) {
-                    console.log('updating isRead of: ', account.id);
-                    await dispatch('UPDATE_STATUS_BY_FIELD', {
-                        uid: account.id,
-                        key: 'isRead',
-                        value: account.isRead,
-                    });
-                }
-            }
+            });
 
         },
 
@@ -113,6 +80,90 @@ const distributors = {
 			if (state.subscriber) {
 				state.subscriber();
 			}
+        },
+
+        async DELETE_RESELLER({}, userDetails) {
+            const { email, id, userObj } = userDetails;
+            try {
+                await axios({
+                    method: 'post',
+                    url: `${process.env.accountManagementURL}/deleteUser`,
+                    data: {
+                        email: email
+                    }
+                });
+                
+                const batchOperation = DB.batch();
+
+                //deleting account details
+                await DB.collection('accounts').doc(id).delete();
+
+                //deleting conversations relate to the branch
+                const convoToBeDeletedRef = await DB.collection('conversations').where('users', 'array-contains', id).get();
+                convoToBeDeletedRef.docs.forEach(convo => {
+                    batchOperation.delete(DB.collection('conversations').doc(convo.id));
+                });
+
+                //delete profile picture related to the branch
+                if(userObj.hasOwnProperty('downloadURL') || userObj.downloadURL) {
+                    await STORAGE.ref('appsell').child('profile-pictures/' + id).delete();
+                }
+
+                //deleting messages related to the branch
+                const messagesToDeleteRef = await DB.collection('messages').where('sender', '==', id).get();
+                messagesToDeleteRef.docs.forEach(doc => {
+                    batchOperation.delete(DB.collection('messages').doc(doc.id));
+                });
+
+                //deleting stockorders related to the branch
+                const stockOrderToDeleteRef = await DB.collection('stock_orders').where('userId', '==', id).get();
+                stockOrderToDeleteRef.docs.forEach(doc => {
+                    batchOperation.delete(DB.collection('stock_orders').doc(doc.id));
+                });
+
+                //deleting shipments related to the branch
+                const shipmentsToDeleteRef = await DB.collection('shipment').where('userDetails.userId', '==', id).get();
+                shipmentsToDeleteRef.docs.forEach(doc => {
+                    batchOperation.delete(DB.collection('shipment').doc(doc.id));
+                });
+
+                //deleting support message related to the branch
+                const supportToDeleteRef = await DB.collection('support').where('uid', '==', id).get();
+                supportToDeleteRef.docs.forEach(doc => {
+                    batchOperation.delete(DB.collection('support').doc(doc.id));
+                });
+
+                await batchOperation.commit();
+
+                return {
+                    isSuccessful: true
+                };
+
+            } catch(error) {
+                console.log(error);
+                throw error;
+            }
+        },
+        
+        async RESEND_ACCOUNT_VERIFICATION({}, userDetails) {
+            const { firstName, email } = userDetails;
+
+            try {
+                const response = await axios({
+                    method: 'post',
+                    url: `${process.env.accountManagementURL}/sendAccountVerification`,
+                    data: {
+                        firstName: firstName,
+                        email: email,
+                        company: process.env.companyName
+                    }
+                });
+
+                return response;
+            
+            } catch(error) {
+                throw error;
+            }
         },
 
         async FIND({ rootState, commit, dispatch }, payload) {
