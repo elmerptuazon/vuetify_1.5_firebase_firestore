51ddd371688f5cc77419da081b4e09fbdb8f5410
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-07-15 13:55:47 +0800

    changed account management URL for Rraw

--- company_setup/Rraw/environmentConfig/test.env.js
+++ company_setup/Rraw/environmentConfig/test.env.js
@@ -10,7 +10,7 @@ module.exports = merge(devEnv, {
   storageBucket: '"rrawdev.appspot.com"',
   messagingSenderId: '"26080521344"',
   appId: '"1:26080521344:web:ed686a294b090493368a65"',
-  primaryColor: '"#FCE4D4"',
+  primaryColor: '"#ccafaf"',
   companyName: '"Rraw"',
   accountManagementURL: '"https://us-central1-rrawdev.cloudfunctions.net/accounts"'
 })
