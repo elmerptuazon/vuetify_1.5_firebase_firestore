468a588e58445fe875c92c982dce6819ccbe5df2
China Lao <china.appsell@gmail.com>
2020-07-23 15:18:52 +0800

    reused Cacao to become brandless

--- company_setup/CacaoCultureFarms/environmentConfig/prod.env.js
+++ company_setup/CacaoCultureFarms/environmentConfig/prod.env.js
@@ -8,7 +8,7 @@ module.exports = {
   messagingSenderId: '"26371903278"',
   appId: '"1:26371903278:web:850ae7581232b3484bb4bf"',
   measurementId: '"G-XV2JER7LNT"',
-  primaryColor: '"#b81212"',
-  companyName: '"Cacao Culture Farms"',
+  primaryColor: '"#508fe6"',
+  companyName: '"Company Name"',
   accountManagementURL: '"https://us-central1-cacaoculturefarms-dev.cloudfunctions.net/accounts"'
 }
